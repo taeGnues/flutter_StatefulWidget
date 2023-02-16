8f929a954378014b155b3b524435283cf88b68b3
정태승 <hi563@g.skku.edu>
2023-02-16 16:56:36 +0900

    first commit

--- lib/Screen/home_screen.dart
+++ lib/Screen/home_screen.dart
@@ -1,92 +1,107 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+// class HomeScreen extends StatefulWidget{
+//   final Color color;
+//
+//   const HomeScreen({
+//     required this.color,
+//     Key? key,
+// }) : super(key: key); // constructor 생성자. 딱 한번 실행.
+//
+//   @override
+//   State<StatefulWidget> createState() { // Stateful Widget의 State를 돌려준다.
+//     return _HomeScreenState(); // State는 private하게 만들어준다.
+//   }
+//
+//
+// }
+//
+// class _HomeScreenState extends State<HomeScreen>{
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       width: 50.0,
+//       height: 50.0,
+//       color: widget.color, // parameter를 state에서 받아옴. state에 있는 color를 가져옴.
+//     );
+//   }
+// }
+
+class HomeScreen extends StatefulWidget {
+  final Color color;
+
+  HomeScreen({
+    required this.color,
+    Key? key,
+  }) : super(key: key) {
+    print('Widget Constructor 실행!');
+  } // 컨스트럭트 생성하자마자 바로 실행
+
+  @override
+  State<HomeScreen> createState() {
+    print('CreateState 실행');
+    return _HomeScreenState();
+  }
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int number = 0;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    print('initState실행');
+  }
+
+  @override
+  void didChangeDependencies() {
+    // TODO: implement didChangeDependencies
+    super.didChangeDependencies();
+
+    print('didChangeDependecies 실행');
+  }
+
+  @override
+  void deactivate() {
+    // TODO: implement deactivate
+    super.deactivate();
+
+    print('deactivate 실행');
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+
+    print('dispose 실행');
+  }
+
+  @override
+  void didUpdateWidget(covariant HomeScreen oldWidget) {
+    // TODO: implement didUpdateWidget
+    print('didUpdateWidget 실행');
+    super.didUpdateWidget(oldWidget);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Container(
-          color: Colors.black,
-          width: MediaQuery.of(context).size.width,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                 crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.max,
-                children: [
-                  Container(
-                    color: Colors.red,
-                    width: 50,
-                    height: 50,
-                  ),
-                  Container(
-                    color: Colors.orange,
-                    width: 50,
-                    height: 50,
-                  ),
-                  Container(
-                    color: Colors.yellow,
-                    width: 50,
-                    height: 50,
-                  ),
-                  Container(
-                    color: Colors.green,
-                    width: 50,
-                    height: 50,
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    color: Colors.orange,
-                    width: 50,
-                    height: 50,
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Container(
-                    color: Colors.red,
-                    width: 50,
-                    height: 50,
-                  ),
-                  Container(
-                    color: Colors.orange,
-                    width: 50,
-                    height: 50,
-                  ),
-                  Container(
-                    color: Colors.yellow,
-                    width: 50,
-                    height: 50,
-                  ),
-                  Container(
-                    color: Colors.green,
-                    width: 50,
-                    height: 50,
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    color: Colors.green,
-                    width: 50,
-                    height: 50,
-                  ),
-                ],
-              )
-            ],
+    print('build 실행');
+    return GestureDetector(
+      onTap: (){
+        setState(() {
+          number++;
+        });
+        },
+      child: Container(
+        width: 50.0,
+        height: 50.0,
+        color: widget.color,
+        child: Center(
+          child: Text(
+            number.toString(),
           ),
         ),
       ),
